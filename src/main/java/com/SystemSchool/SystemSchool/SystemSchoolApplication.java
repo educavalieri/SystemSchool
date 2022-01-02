9b77bfe66f2ec3a4671c3a12ddb13c958c243bf1
educavalieri <eduardocavalieri@hotmail.com>
2022-01-02 18:02:49 -0300

    starting project

---
+++ src/main/java/com/SystemSchool/SystemSchool/SystemSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.SystemSchool.SystemSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SystemSchool/SystemSchool/SystemSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SystemSchool.SystemSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
